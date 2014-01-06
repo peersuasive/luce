ecaa5faa5fd8c07e7d86923b0051736faf82eda9
Christophe Berbizier (dbdl) <cberbizier@peersuasive.com>
2014-01-06 18:32:06 +0100

    LMainComponent: bound all mouse* to LComponent

--- Source/LMainComponent.cpp
+++ Source/LMainComponent.cpp
@@ -40,3 +40,39 @@ void LMainComponent::paint(Graphics& g) {
 void LMainComponent::resized() {
     LComponent::lresized();
 }
+
+void LMainComponent::mouseMove(const MouseEvent& e) {
+    LComponent::lmouseMove(e);
+}
+
+void LMainComponent::mouseEnter(const MouseEvent& e) {
+    LComponent::lmouseEnter(e);
+}
+
+void LMainComponent::mouseExit(const MouseEvent& e) {
+    LComponent::lmouseExit(e);
+}
+
+void LMainComponent::mouseDown(const MouseEvent& e) {
+    LComponent::lmouseDown(e);
+}
+
+void LMainComponent::mouseDrag(const MouseEvent& e) {
+    LComponent::lmouseDrag(e);
+}
+
+void LMainComponent::mouseUp(const MouseEvent& e) {
+    LComponent::lmouseUp(e);
+}
+
+void LMainComponent::mouseDoubleClick(const MouseEvent& e) {
+    LComponent::lmouseDoubleClick(e);
+}
+
+void LMainComponent::mouseWheelMove (const MouseEvent& e, const MouseWheelDetails& wheel) {
+    LComponent::lmouseWheelMove(e, wheel);
+}
+
+void LMainComponent::mouseMagnify (const MouseEvent& e, float scaleFactor) {
+    LComponent::lmouseMagnify(e, scaleFactor);
+}
--- Source/LMainComponent.h
+++ Source/LMainComponent.h
@@ -33,6 +33,16 @@ private:
     virtual void paint (Graphics& g) override;
     virtual void resized () override;
 
+    virtual void mouseMove (const MouseEvent&) override;
+    virtual void mouseEnter (const MouseEvent&) override;
+    virtual void mouseExit (const MouseEvent&) override;
+    virtual void mouseDown (const MouseEvent&) override;
+    virtual void mouseDrag (const MouseEvent&) override;
+    virtual void mouseUp (const MouseEvent&) override;
+    virtual void mouseDoubleClick (const MouseEvent&) override;
+    virtual void mouseWheelMove (const MouseEvent&, const MouseWheelDetails&) override;
+    virtual void mouseMagnify (const MouseEvent&, float scaleFactor) override;
+
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (LMainComponent)
 };
