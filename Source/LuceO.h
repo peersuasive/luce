69ce81ef74e96ed38c445148084ad014b7ba2ca7
Christophe Berbizier (dbdl) <cberbizier@peersuasive.com>
2014-02-21 19:29:57 +0100

    removed duplicate

--- Source/LuceO.h
+++ Source/LuceO.h
@@ -27,8 +27,6 @@ namespace {
     template<class T>
     const juce::Point<T> luce_topoint(int i = -1);
 
-    template<class T>
-    const juce::Rectangle<T> luce_torectangle(int i = -1);
 
     template<class T>
     const juce::Range<T> luce_torange(int i = -1);
