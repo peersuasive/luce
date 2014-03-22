99539484ae470d42ae9aa16d2e901e367b90f6c7
Christophe Berbizier (dbdl) <cberbizier@peersuasive.com>
2014-03-22 18:57:08 +0100

    luce/LRange: renamed rstart/rend to lstart/lend, implemented dump/tostring

--- Source/lua/luce/LRange.lua
+++ Source/lua/luce/LRange.lua
@@ -19,84 +19,84 @@ local className = "LRange"
 local mt = {}
 
 function mt:getStart()
-    return self.rstart
+    return self.lstart
 end
 
 function mt:getEnd()
-    return self.rend
+    return self.lend
 end
 
 function mt:getLength()
-    return self.rend - self.rstart
+    return self.lend - self.lstart
 end
 
 function mt:isEmpty()
-    return self.rstart == rend
+    return self.lstart == lend
 end
 
 function mt:setStart(s)
-    self.rstart = s
-    if(self.rend < s)then self.rend = s end
+    self.lstart = s
+    if(self.lend < s)then self.lend = s end
     return self
 end
 
 function mt:withStart(s)
-    return self:new(s, math.max(s, self.rend))
+    return self:new(s, math.max(s, self.lend))
 end
 
 function mt:movedToStartAt(s)
-    return self:new(s, self.rend+(s-self.rstart))
+    return self:new(s, self.lend+(s-self.lstart))
 end
 
 function mt:setEnd(e)
-    self.rend = e
-    if(self.e<self.rstart)then
-        self.rstart = e
+    self.lend = e
+    if(self.e<self.lstart)then
+        self.lstart = e
     end
     return self
 end
 
 function mt:withEnd(e)
-    return self:new(math.min(self.rstart, e), e)
+    return self:new(math.min(self.lstart, e), e)
 end
 
 function mt:movedToEndAt(e)
-    return self:new(self.rstart+(e-self.rend), e)
+    return self:new(self.lstart+(e-self.lend), e)
 end
 
 function mt:setLength(l)
-    self.rend = self.rstart + math.max(0, l)
+    self.lend = self.lstart + math.max(0, l)
     return self
 end
 
 function mt:withLength(l)
-    return self:new(self.rstart, self.rstart+l)
+    return self:new(self.lstart, self.lstart+l)
 end
 
 function mt:contains(pos)
     if(pos.__type and assert(pos.__type == "LRange"))then
-        return self.rstart <= pos.rstart and self.rend >= pos.rend
+        return self.lstart <= pos.lstart and self.lend >= pos.lend
     else
-        return self.rstart <= pos and pos < self.rend
+        return self.lstart <= pos and pos < self.lend
     end
 end
 
 function mt:clipValue(val)
-    return (val<self.rstart) 
-            and self.rstart
-            or ( (self.rend < val) and self.rend or val )
+    return (val<self.lstart) 
+            and self.lstart
+            or ( (self.lend < val) and self.lend or val )
 end
 
 function mt:intersect(r)
-    return r.rstart < self.rend and self.rstart < r.rend
+    return r.lstart < self.lend and self.lstart < r.lend
 end
 
 function mt:getIntersectionWith(r)
-    return self:new( math.max(self.rstart, r.rstart), math.min(self.rend, r.rend) )
+    return self:new( math.max(self.lstart, r.lstart), math.min(self.lend, r.lend) )
 end
 
 function mt:getUnionWith(r)
-    return self:new( math.min(self.rstart, r.start), math.max(self.rend, r.rend) )
+    return self:new( math.min(self.lstart, r.start), math.max(self.lend, r.lend) )
 end
 
 function mt:constrainRange(r)
@@ -104,35 +104,47 @@ function mt:constrainRange(r)
     if(self:getLength() <= l)then
         return self
     else
-        local limSup = self.rend - l
-        return (r.rstart < self.rstart) 
-                and self.rstart
-                or ( (limSup < r.rstart) and limSup or r.rstart )
+        local limSup = self.lend - l
+        return (r.lstart < self.lstart) 
+                and self.lstart
+                or ( (limSup < r.lstart) and limSup or r.lstart )
     end
 end
 
+function mt:dump()
+    return { self.lstart, self.lend, __type = self.__type }
+end
 
 local function new(_, ...)
-    local rstart, rend = 0, 0
+    local lstart, lend, __type = 0, 0, "int"
     local n = #{...}
     if n >= 2 then
-        local b, e = ...
-        rstart = b
-        rend = math.max(b, e)
+        local b, e, t = ...
+        lstart = b
+        lend   = math.max(b, e)
+        __type = t
     elseif n == 1 then
         local r = ...
-        rstart = r:getStart()
-        rend = r:getEnd()
+        lstart = r:getStart()
+        lend   = r:getEnd()
+        __type = r.__type
     end
     
     local self = {
-        rstart = rstart,
-        rend   = rend,
+        lstart = lstart,
+        lend   = lend,
     }
+    self.__type = __type or "int"
+    self.__ltype = className
+
     self.new = mt.new
     return setmetatable(self, {
-        __index = self,
-        -- __eq, __add, moins...
+        __self = className,
+        __index = mt,
+        __tostring = function()
+            return string.format("%s %s", self.lstart, self.lend)
+        end,
+        -- __eq, __add,...
     })
 end
 mt.new = new
