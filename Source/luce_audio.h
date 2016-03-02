d460893a45fe71a52a7cf5ebf3efc7442ba278d7
Christophe Berbizier (dbdl) <cberbizier@peersuasive.com>
2016-03-02 22:09:22 +0100

    fixed audio compilation - removed unused header

--- Source/luce_audio.h
+++ Source/luce_audio.h
@@ -1,13 +1,8 @@
 #ifndef __LUCE_AUDIO_H
 #define __LUCE_AUDIO_H
 
-//#include "luce.h"
-//namespace luce {
-
 #include "audio/LAudioAppComponent.h"
 #include "audio/LAudioSampleBuffer.h"
-#include "base/LPointer.h"
-
-//}
+//#include "base/LPointer.h"
 
 #endif // __LUCE_AUDIO_H
